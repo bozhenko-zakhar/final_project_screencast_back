931b58cc037c373b58c7514f06414e1902bd69a6
laven97 <poleschucke@gmail.com>
2026-04-24 14:39:21 +0200

    review

--- src/controllers/auth/registerUser.js
+++ src/controllers/auth/registerUser.js
@@ -1,26 +1,3 @@
-import { User } from '../../models/user.js';
-import { createSession, setSessionCookies } from '../../services/auth.js';
-
-import createHttpError from 'http-errors';
-import bcrypt from 'bcrypt';
-
-
-export const registerUser = async (req, res) => {
-  const { email, password } = req.body;
-
-  const existingUser = await User.findOne({ email });
-
-  if (existingUser) {
-    throw createHttpError(400, 'Email in use');
-  }
-
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = await User.create({
-    email,
-    password: hashedPassword,
-  });
-  const newSession = await createSession(user._id);
-  setSessionCookies(res, newSession);
-
-  res.status(201).json(user);
+export const registerUser =  (req, res) => {
+  res.json("User created");
 };
