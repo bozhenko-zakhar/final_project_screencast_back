b4dd9a35761cf00d94ef48e8e01642710d909a46
Viacheslav-Bo <zotikivna@gmail.com>
2026-04-24 18:51:21 +0300

    change controller logic

--- src/controllers/tasks/changeTask.js
+++ src/controllers/tasks/changeTask.js
@@ -2,29 +2,21 @@ import { Task } from "../../models/task.js";
 import createHttpError from "http-errors";
 
 export const changeTask = async (req, res) => {
-  try {
-    const { taskId } = req.params;
-    const { isDone } = req.body;
+  const { taskId } = req.params;
+  const { isDone } = req.body;
 
-    const task = await Task.findOneAndUpdate(
-      { _id: taskId, userId: req.user._id },
-      { isDone },
-      { new: true },
-    );
+  const task = await Task.findOneAndUpdate(
+    { _id: taskId, userId: req.user._id },
+    { isDone },
+    {
+      new: true,
+      runValidators: true,
+    },
+  );
 
-    if (!task) {
-      throw createHttpError(404, "Task not found");
-    }
-
-    const safeTask = {
-      _id: task._id,
-      name: task.name,
-      date: task.date,
-      isDone: task.isDone,
-    };
-
-    res.status(200).json({ data: safeTask });
-  } catch {
-    res.status(500).json({ message: "Server error" });
+  if (!task) {
+    throw createHttpError(404, "Task not found");
   }
+
+  res.status(200).json({ data: task });
 };
