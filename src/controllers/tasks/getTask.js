3d6d2c7e32fbd08782c96918fa12fc2635bd9059
Sasha <sasha21100@ukr.net>
2026-04-26 23:48:17 +0300

    getTask.js

--- src/controllers/tasks/getTask.js
+++ src/controllers/tasks/getTask.js
@@ -1,16 +1,3 @@
-import { Task } from "../../models/task.js";
-
-export async function getTasks(req, res) {
-  try {
-    const tasks = await Task.find({ owner: req.user._id }).sort({
-      createdAt: -1,
-    });
-
-    res.json(tasks);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-}
 
 import { Task } from "../../models/task.js";
 
