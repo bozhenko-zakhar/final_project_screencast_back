5ff9ab9934b8f1f5e4d3c469def338fb264e250b
laven97 <poleschucke@gmail.com>
2026-04-30 17:51:25 +0200

    refreshAuthenticate

---
+++ src/middleware/refreshAuthenticate.js
@@ -0,0 +1,49 @@
+import createHttpError from 'http-errors';
+
+import { Session } from '../models/session.js';
+import { User } from '../models/user.js';
+import { generateTokens } from '../services.js';
+import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/time.js';
+import { setSessionCookies } from '../services/auth.js';
+
+export const refreshAuthenticate = async (req, res, next) => {
+  try {
+    const { refreshToken } = req.cookies;
+
+    if (!refreshToken) {
+      throw createHttpError(401, 'Mising refresh token');
+    }
+
+    const session = await Session.findOne({ refreshToken });
+    if (!session) {
+      throw createHttpError(401, 'Session not found');
+    }
+
+    const isExpired =
+      Date.now() > new Date(session.refreshTokenValidUntil).getTime();
+    if (isExpired) {
+      await Session.deleteOne({ _id: session._id });
+      throw createHttpError(401, 'Refresh token expired');
+    }
+
+    const user = await User.findById(session.userId).select('-password');
+    if (!user) {
+      throw createHttpError(401, 'User not found');
+    }
+
+    const tokens = generateTokens();
+
+    session.accessToken = tokens.accessToken;
+    session.refreshToken = tokens.refreshToken;
+    session.accessTokenValidUntil = new Date(Date.now() + FIFTEEN_MINUTES);
+    session.refreshTokenValidUntil = new Date(Date.now() + ONE_DAY);
+
+    await session.save();
+
+    setSessionCookies(res, session);
+
+    return res.json({ user });
+  } catch (err) {
+    next(err);
+  }
+};
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -2,12 +2,13 @@ import { Router } from 'express';
 
 import { auth } from '../controllers/index.js';
 import { authenticate } from '../middleware/authenticate.js';
+import { refreshAuthenticate } from '../middleware/refreshAuthenticate.js';
 
 const authRoute = Router();
 
 authRoute.post('/login', auth.loginUser);
 authRoute.post('/logout', authenticate, auth.logoutUser);
-authRoute.post('/refresh', authenticate, auth.refreshUserSession);
+authRoute.post('/refresh', refreshAuthenticate, auth.refreshUserSession);
 authRoute.post('/register', auth.registerUser);
 
 export default authRoute;
