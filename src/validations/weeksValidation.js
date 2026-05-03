4ae16870004859f944f9bac4f2dad9dfb76e3290
Artemmm008 <artemshkola141@gmail.com>
2026-05-03 15:55:05 +0100

    fixed

--- src/validations/weeksValidation.js
+++
@@ -1,9 +0,0 @@
-import { Joi } from 'celebrate';
-import { Segments } from "celebrate";
-
-export const getWeekSchema = {
-    [Segments.QUERY]: Joi.object({
-        weekNumber: Joi.number().min(1).max(40).required(),
-        dueDate: Joi.date().iso()
-    }),
-};
